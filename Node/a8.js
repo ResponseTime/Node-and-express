5eb746b4bf53d1075680599fcef891cc78fa3a43
Looper <aayush.bhattacharjee2002@gmail.com>
2022-11-06 15:00:17 +0530

    node part

--- Node/a8.js
+++ Node/a8.js
@@ -6,14 +6,14 @@
     transform
 */
 const fs = require('fs')
-const read = fs.createReadStream('demo.txt',{highWaterMark:90000,encoding:'utf8'})
-const write = fs.createWriteStream('demo.txt',{highWaterMark:90000,encoding:'utf8',flags:'a'})
+const read = fs.createReadStream('demo.txt', { highWaterMark: 90000, encoding: 'utf8' })
+const write = fs.createWriteStream('demo.txt', { highWaterMark: 90000, encoding: 'utf8', flags: 'a' })
 // write.write(" hello wrold")
-read.on('data',(res)=>{
+read.on('data', (res) => {
     console.log(res)
 })
-read.on('error',(err)=>{
+read.on('error', (err) => {
     console.error(err)
 })
 
-read.emit('data',"my name is ")
+read.emit('data', "my name is ")
