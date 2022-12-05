6b8f0ccbfa2883bbbc2eb74d8857b4d9d13424be
Looper <aayush.bhattacharjee2002@gmail.com>
2022-12-05 10:48:42 +0530

    Express part

--- Express/exp13.js
+++ Express/exp13.js
@@ -1,19 +1,24 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-app.use(express.urlencoded({extended:false}))
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname,'/test.html'))
-})
-app.post('/login', (req, res)=>{
-    let {name} = req.body
-    if(name){
-        res.send(`Hello ${name}`)
-    }
-    else{
-        res.status(401).send("No name")
-    }
-})
-app.listen(5000,()=>
-    console.log('5000')
-)
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/test.html"));
+});
+// app.post("/login", (req, res) => {
+//   let { name } = req.body;
+//   if (name) {
+//     res.send(`Hello ${name}`);
+//   } else {
+//     res.status(401).send("No name");
+//   }
+// });
+app.get("/login", (req, res) => {
+  let { name } = req.query;
+  if (name) {
+    res.send(`Hello ${name}`);
+  } else {
+    res.status(401).send("No name");
+  }
+});
+app.listen(5000, () => console.log("5000"));
